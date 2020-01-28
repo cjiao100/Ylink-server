43fd71a7c0944e1627af28ba1a60116e995e8e20
cjiao100 <1971416012@qq.com>
2020-01-28 22:08:34 +0800

    初始化

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  res.header('Access-Control-Allow-Methods', 'PUT,POST,GET,DELETE,OPTIONS');
+  next();
+});
+
+// 使用bodyParser
+app.use(
+  bodyParser.urlencoded({
+    extended: false,
+  }),
+);
+
+app.get('/', (req, res) => {
+  res.send('Hello Word');
+});
+
+app.listen(port, () => {
+  console.log(`Server is runing on port ${port}`);
+});
