1085eed1ee3190da3de080abe23a31ecef9e73dd
cjiao100 <1971416012@qq.com>
2020-05-17 12:11:30 +0800

    修改搜索接口

--- routes/search.js
+++ routes/search.js
@@ -3,18 +3,23 @@ const passport = require('passport');
 
 const Article = require('../models/article');
 const Post = require('../models/post');
+const User = require('../models/user');
 const router = express.Router();
 
-router.get(
+router.post(
   '/',
   passport.authenticate('jwt', { session: false }),
   async (req, res) => {
     const query = req.body.query;
     const result = await Promise.all([
       Article.find({ title: { $regex: query } }),
-      Post.find({ title: { $regex: query } }),
+      Post.find({ title: { $regex: query } }).populate({
+        path: 'userId',
+        model: User,
+        select: { name: 1, avatar: 1 },
+      }),
     ]);
-    res.json(result);
+    res.json({ success: true, data: result });
   },
 );
 
